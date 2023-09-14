e47df78aac30998e3cf7e60430a84e015bacf7ce
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-09-14 21:28:00 +0600

    successfully fetched data from json file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import Cards from "../Cards/Cards";
 
 const Main = () => {
+    const [cards,setCards] =useState([])
+    useEffect(()=>{
+        fetch("/public/data.json")
+        .then(res => res.json())
+        .then(data =>setCards(data))
+    },[])
   return (
     <div className="container mx-auto">
       <Cards></Cards>
